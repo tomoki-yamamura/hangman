cfd6304835f8515c8c63ca5cdcd36ae59118639b
tomoki-yamamura <progtab17@gmail.com>
2024-04-16 19:41:26 +0800

    add Quiz class

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,8 +1,31 @@
-import rawData from "./data/questions.test.json"
+import rawData from "./data/questions.test.json";
 
 interface Question {
   word: string;
-  hint: string
+  hint: string;
 }
 
-const questions: Question[] = rawData
+const questions: Question[] = rawData;
+
+class Quiz {
+  questions: Question[];
+  constructor(questions: Question[]) {
+    this.questions = questions;
+  }
+
+  getNext(): Question {
+    const idx = Math.floor(Math.random() * this.questions.length);
+    const [question] = this.questions.splice(idx, 1);
+    return question;
+  }
+
+  hasNext(): boolean {
+    return this.questions.length > 0;
+  }
+
+  lefts(): number {
+    return this.questions.length;
+  }
+}
+
+const quiz = new Quiz(questions);
