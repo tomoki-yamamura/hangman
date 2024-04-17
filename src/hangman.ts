d89dad4ebbeda01e4e9066a0f445e26a23f3c2ee
tomoki-yamamura <progtab17@gmail.com>
2024-04-17 22:57:38 +0800

    fin Stage

--- src/hangman.ts
+++ src/hangman.ts
@@ -78,4 +78,44 @@ class Quiz {
   }
 }
 
+class Stage {
+  answer: string;
+  leftAttempts: number = 5;
+  question: Question;
+  constructor(question:Question) {
+    this.question = question;
+    this.answer = new Array(question.word.length).fill("_").join("")
+  }
+
+  updateAnswer(userInput: string= ""): void {
+    if (!userInput) return
+    const regex = new RegExp(userInput, "g")
+    const answerArry = this.answer.split("");
+    let matches: RegExpExecArray | null;
+    while((matches = regex.exec(this.question.word))) {
+      const foundIdx = matches.index;
+      answerArry.splice(foundIdx, userInput.length, ...userInput)
+      this.answer = answerArry.join("")
+    }
+  }
+
+  isTooLong(userInput: string): boolean {
+    return userInput.length > this.question.word.length;
+  }
+  isIncludes(userInput: string): boolean {
+    return this.question.word.includes(userInput)
+  }
+  isCorrect(): boolean {
+    return this.answer === this.question.word
+  }
+
+  decrementAttempts(): number {
+    return --this.leftAttempts;
+  }
+
+  isGameOver(): boolean {
+    return this.leftAttempts === 0;
+  }
+}
+
 const quiz = new Quiz(questions);
