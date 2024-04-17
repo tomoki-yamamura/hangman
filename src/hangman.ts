297fbc666cc0740a6e3a8618be405f345400aba5
tomoki-yamamura <progtab17@gmail.com>
2024-04-17 23:14:45 +0800

    fin Message

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,7 +1,7 @@
 import rawData from "./data/questions.test.json";
-import readlinePromiss from "readline/promises"
+import readlinePromiss from "readline/promises";
 import chalk from "chalk";
-import figlet from "figlet"
+import figlet from "figlet";
 
 type Color = "red" | "green" | "yellow" | "white";
 
@@ -23,12 +23,12 @@ interface UserInterface {
 const rl = readlinePromiss.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
 const CLI: UserInterface = {
   async input() {
     const input = await rl.question("Guess the word or letter: ");
-    return input.replaceAll(" ","").toLowerCase();
+    return input.replaceAll(" ", "").toLowerCase();
   },
 
   clear() {
@@ -36,7 +36,7 @@ const CLI: UserInterface = {
   },
 
   destroy() {
-    rl.close()
+    rl.close();
   },
 
   output(message: string, color: Color = "white") {
@@ -44,15 +44,15 @@ const CLI: UserInterface = {
   },
 
   outputAnswer(message: string) {
-    console.log(figlet.textSync(message, { font: "Big"}), "\n");
-  }
-}
+    console.log(figlet.textSync(message, { font: "Big" }), "\n");
+  },
+};
 
 async function testQuestion() {
   CLI.clear();
-  const userinput = await CLI.input()
+  const userinput = await CLI.input();
   console.log(userinput);
-  CLI.destroy()
+  CLI.destroy();
 }
 
 testQuestion();
@@ -82,20 +82,20 @@ class Stage {
   answer: string;
   leftAttempts: number = 5;
   question: Question;
-  constructor(question:Question) {
+  constructor(question: Question) {
     this.question = question;
-    this.answer = new Array(question.word.length).fill("_").join("")
+    this.answer = new Array(question.word.length).fill("_").join("");
   }
 
-  updateAnswer(userInput: string= ""): void {
-    if (!userInput) return
-    const regex = new RegExp(userInput, "g")
+  updateAnswer(userInput: string = ""): void {
+    if (!userInput) return;
+    const regex = new RegExp(userInput, "g");
     const answerArry = this.answer.split("");
     let matches: RegExpExecArray | null;
-    while((matches = regex.exec(this.question.word))) {
+    while ((matches = regex.exec(this.question.word))) {
       const foundIdx = matches.index;
-      answerArry.splice(foundIdx, userInput.length, ...userInput)
-      this.answer = answerArry.join("")
+      answerArry.splice(foundIdx, userInput.length, ...userInput);
+      this.answer = answerArry.join("");
     }
   }
 
@@ -103,10 +103,10 @@ class Stage {
     return userInput.length > this.question.word.length;
   }
   isIncludes(userInput: string): boolean {
-    return this.question.word.includes(userInput)
+    return this.question.word.includes(userInput);
   }
   isCorrect(): boolean {
-    return this.answer === this.question.word
+    return this.answer === this.question.word;
   }
 
   decrementAttempts(): number {
@@ -115,6 +115,46 @@ class Stage {
 
   isGameOver(): boolean {
     return this.leftAttempts === 0;
+  }
+}
+
+class Message {
+  ui: UserInterface;
+  constructor(ui: UserInterface) {
+    this.ui = ui;
+  }
+
+  askQuestion(stage: Stage): void {
+    this.ui.output(`Hint: ${stage.question.hint}`, "yellow");
+    this.ui.outputAnswer(stage.answer.replaceAll("", " ").trim());
+    this.ui.output(`(残りの試行回数: ${stage.leftAttempts})`);
+  }
+  leftQuestions(quiz: Quiz) {
+    this.ui.output(`${quiz.lefts() + 1}s left`);
+  }
+  start() {
+    this.ui.output("\nGame Start!!");
+  }
+  enterSomething() {
+    this.ui.output(`Try input!`, "red");
+  }
+  notInclude(input: string) {
+    this.ui.output(`word doesn't have "${input}" `, "red");
+  }
+  notCorrect(input: string) {
+    this.ui.output(`"${input}" is wrong`, "red");
+  }
+  hit(input: string) {
+    this.ui.output(`"${input}" is Hit!`, "green");
+  }
+  correct(question: Question) {
+    this.ui.output(`The correct is "${question.word}" `, "green");
+  }
+  gameover(question: Question) {
+    this.ui.output(`The correct is ${question.word}`);
+  }
+  end() {
+    this.ui.output("Good game. Thank you!");
   }
 }
 
